1359215e778200e25c92324258e7b5c33af3aafa
Dev <sjennings.inc@gmail.com>
2016-05-04 14:08:59 +1200

    Initial commit

---
+++ cal.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+$("button").click(function(e) {
+console.log((this.id).text());
+});
+ 
+});
+
+//click function
+//picks up the ID of the item clicked on this. Store 
+
+//multiply function(a, b) {
+//    a * b
+
+// }
+
+// //divide function(a, b) {
+//  return a / b
+// }
+
+// //plus function(a, b) {
+//   return 
+// }
+
+// //minus function
+
+var memory = '0';
+var current = '0';
+var operation;
+var maxLength = 30;
+
+// if (current > maxLength) {
+//   return "Too Long"
+// }
